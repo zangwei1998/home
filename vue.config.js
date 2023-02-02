8a91e4ec3ba5101e62a53277748abce105ae9310
zangwei1998 <1185780575@qq.com>
2023-02-02 10:58:00 +0800

    guifan

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+  transpileDependencies: true
+})
+
+module.exports = {
+  // eslint-disable-next-line prettier/prettier
+  configureWebpack: config => {
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+    config.plugins.push(
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+  }
+}
